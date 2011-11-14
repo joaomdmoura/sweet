04899a778ad6848a232727b8b5020154749ea042
João Moura <joaomdmoura@gmail.com>
2011-11-14 00:41:26 -0200

    Improving lexer to work with complex indentation

--- lexer.rb
+++ lexer.rb
@@ -24,27 +24,31 @@ class Lexer
 			elsif string = chunk[/\A"(.*?)"/, 1]
 				tokens << [:STRING, string]
 				i += string.size + 2
-			elsif indent = chunk[/\A\:\n(	+)/m, 1]
-				if indent.size <= current_indent
-					raise "Bad indent level, got #{indent.size} indents, " +
-								"expected > #{current_indent}"
-				end
-				current_indent = indent.size
-				indent_stack.push(current_indent)
-				tokens << [:INDENT, indent.size]
-				i += indent.size + 2
-			elsif indent = chunk[/\A\n( *)/m, 1]
-				if indent.size == current_indent
-					tokens << [:NEWLINE, "\n"]
+			elsif indent = chunk[/\A\n(	*)/m, 1]
+				if indent.size > current_indent
+					current_indent = indent.size
+					indent_stack.push(current_indent)
+					tokens << [:INDENT, indent.size]
+					i += indent.size + 1
 				elsif indent.size < current_indent
-					indent_stack.pop
-					current_indent = indent_stack.first || 0
-					tokens << [:DEDENT, indent.size]
+					lol = chunk.split("\n")
+					if lol[0] == "" && lol[1] == ""
+						tokens << [:NEWLINE, "\n"]
+						i += indent.size + 1
+					else
+						times = current_indent - indent.size
+						for passed in 1..times.to_i
+							indent_stack.pop
+						end
+						current_indent = indent_stack.last || 0
+						tokens << [:DEDENT, indent.size]
+						tokens << [:NEWLINE, "\n"]
+						i += indent.size + 1
+					end
+				elsif indent.size == current_indent
 					tokens << [:NEWLINE, "\n"]
-				else
-					raise "Missing ':'"
+					i += indent.size + 1
 				end
-				i += indent.size + 1
 			elsif chunk.match(/\A /)
 				i += 1
 			else
