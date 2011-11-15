fd2c0bff064c15151a00819ce7f63b28fe7c52cf
João Moura <joaomdmoura@gmail.com>
2011-11-15 06:05:35 -0200

    adding the first sweet exec a example.s file

---
+++ example.s
@@ -0,0 +1,10 @@
+class Sweet:
+	def does_it_work:
+		"Uhmm this is so Sweet!"
+
+sweet_object = Sweet.new
+if sweet_object:
+	print("sweet_object.does_it_work: ")
+	print(sweet_object.does_it_work)
+else:
+	print("Something is wrong...")
