fdbdb52654083a6f177e776c7ce45d5ff227545f
João Moura <joaomdmoura@gmail.com>
2011-11-24 01:08:03 -0200

    Updating lexer to work with div tag and multiple indentation logic and scope

--- lexer.rb
+++ lexer.rb
@@ -1,61 +1,104 @@
 class Lexer
-	KEYWORDS = ["def", "class", "if", "else", "true", "false", "nil"]
+  KEYWORDS = ["def", "class", "if", "true", "false", "nil"]
+  
+  def tokenize(code)
+    # Cleanup code by remove extra line breaks
+    code.chomp!
 
-	def tokenize(code)
-		code.chomp!
-		# Replacing all spaces, inputing tabs
+    # Replacing all spaces, inputing tabs
 		code = code.gsub( "\n ", "\n\t" )
 		code = code.gsub( /^\s*$/m, '' )
-		
-		general = code.scan(/(div(\n\t.*)+)/)
-		general.each_with_index do |block, index|
-			content = block[0].scan(/(\t+[a-zA-Z0-9]+.*)/)
-			new_def = "def tag_#{index}\n\tprint(\"<div>\")"
+    
+		def convert_tag(code, index, indent)
+			content = code.scan(/(\t+[a-zA-Z0-9]+.*)/)
+			tab = ""
+			p_tab = ""
+			for i in 0..indent
+				if i == (indent - 1 )
+					p_tab = tab
+				else
+					tab += "\t"
+				end
+			end
+			new_def = "def tag_#{index}\n#{tab}print(\"<div>\")"
 			content.each do |lol|
 				new_def += "\n" + lol[0]
 			end
-			new_def += "\n\tprint(\"</div>\")\ntag_#{index}"
-
-			code = code.gsub(block[0], new_def)
+			new_def += "\n#{tab}print(\"</div>\")\n#{p_tab}tag_#{index}"	
+			return new_def
 		end
 
-		i = 0
-		tokens = []
-		current_indent = 0
-		indent_stack = []
-		
-		while i < code.size
-			chunk = code[i..-1]
-			# Matching if, print, method names, etc.
-			if identifier = chunk[/\A([a-z]\w*)/, 1]
-				if KEYWORDS.include?(identifier)
-					tokens << [identifier.upcase.to_sym, identifier]
-				# Non-keyword identifiers include method and variable names.
-				else
-					tokens << [:IDENTIFIER, identifier]
+		def implement_tag(code, indent=1)
+			general = code.scan(/(div(\n\t.*)+)/)
+			general.each_with_index do |block, index|
+				last = block[0]
+				
+				if recrusive_general = block[0].match(/(\n\t+div(\n\t.*)+)/)
+					new_indent = indent + 1
+					new_block = implement_tag(recrusive_general[0], new_indent)
+					block[0] = block[0].gsub(recrusive_general[0], new_block)
 				end
-				# skip what was just parsed
-				i += identifier.size
-			
-			# Matching class names and constants starting with a capital letter.
-			elsif constant = chunk[/\A([A-Z]\w*)/, 1]
-				tokens << [:CONSTANT, constant]
-				i += constant.size
-				
-			elsif number = chunk[/\A([0-9]+)/, 1]
-				tokens << [:NUMBER, number.to_i]
-				i += number.size
-				
-			elsif string = chunk[/\A"(.*?)"/, 1]
-				tokens << [:STRING, string]
-				i += string.size + 2
-			
-			# Here's the indentation magic!
-			elsif indent = chunk[/\A\n(\t+)/m, 1] # Matches ": <newline> <spaces>"
+				new_def = convert_tag( block[0] , index, indent)
+				code = code.gsub(last, new_def)
+			end
+			return code
+		end
+
+		code = implement_tag(code)
+
+    # Current character position we're parsing
+    i = 0
+    
+    # Collection of all parsed tokens in the form [:TOKEN_TYPE, value]
+    tokens = []
+    
+    # Current indent level is the number of spaces in the last indent.
+    current_indent = 0
+    # We keep track of the indentation levels we are in so that when we dedent, we can 
+    # check if we're on the correct level.
+    indent_stack = []
+    
+    # This is how to implement a very simple scanner.
+    # Scan one character at the time until you find something to parse.
+    while i < code.size
+      chunk = code[i..-1]
+      
+      # Matching standard tokens.
+      #
+      # Matching if, print, method names, etc.
+      if identifier = chunk[/\A([a-z]\w*)/, 1]
+        # Keywords are special identifiers tagged with their own name, 'if' will result
+        # in an [:IF, "if"] token
+        if KEYWORDS.include?(identifier)
+          tokens << [identifier.upcase.to_sym, identifier]
+        # Non-keyword identifiers include method and variable names.
+        else
+          tokens << [:IDENTIFIER, identifier]
+        end
+        # skip what we just parsed
+        i += identifier.size
+      
+      # Matching class names and constants starting with a capital letter.
+      elsif constant = chunk[/\A([A-Z]\w*)/, 1]
+        tokens << [:CONSTANT, constant]
+        i += constant.size
+        
+      elsif number = chunk[/\A([0-9]+)/, 1]
+        tokens << [:NUMBER, number.to_i]
+        i += number.size
+        
+      elsif string = chunk[/\A"(.*?)"/, 1]
+        tokens << [:STRING, string]
+        i += string.size + 2
+      
+     	# Here's the indentation magic!
+			elsif indent = chunk[/\A\n(\t+)/m, 1]
 				# Create a new block we expect the indent level to go up.
 				if indent.size < current_indent
-					raise "Bad indent level, got #{indent.size} indents, " +
-								"expected > #{current_indent}"
+					indent_stack.pop
+					current_indent = indent_stack.last || 0
+					tokens << [:DEDENT, indent.size]
+					tokens << [:NEWLINE, "\n"]
 								
 				elsif indent.size == current_indent
 					tokens << [:NEWLINE, "\n"]
@@ -69,40 +112,44 @@ class Lexer
 
 				i += indent.size + 1
 
-			elsif indent = chunk[/\A\n(	*)/m, 1] # Matches "<newline> <spaces>"
+			elsif indent = chunk[/\A\n(	*)/m, 1]
 				if indent.size == current_indent
 					# Nothing to do, we're still in the same block
 					tokens << [:NEWLINE, "\n"]
 				elsif indent.size < current_indent
 					indent_stack.pop
-					current_indent = indent_stack.first || 0
+					current_indent = indent_stack.last || 0
 					tokens << [:DEDENT, indent.size]
 					tokens << [:NEWLINE, "\n"]
-				else
-					# Cannot increase indent level without using ":", so this is an error.
-					raise "Missing ':'"
 				end
 				i += indent.size + 1
-			
-			# Ignore whitespace
-			elsif chunk.match(/\A /)
-				i += 1
-			
-			# Treat all other single characters as a token. Eg.: ( ) , . !
-			else
-				value = chunk[0,1]
-				tokens << [value, value]
-				i += 1
-				
-			end
-			
-		end
-		
-		# Close all open blocks
-		while indent = indent_stack.pop
-			tokens << [:DEDENT, indent_stack.first || 0]
-		end
-		
-		tokens
-	end
+      
+      # Match long operators such as ||, &&, ==, !=, <= and >=.
+      # One character long operators are matched by the catch all `else` at the bottom.
+      elsif operator = chunk[/\A(\|\||&&|==|!=|<=|>=)/, 1]
+        tokens << [operator, operator]
+        i += operator.size
+      
+      # Ignore whitespace
+      elsif chunk.match(/\A /)
+        i += 1
+      
+      # Catch all single characters
+      # We treat all other single characters as a token. Eg.: ( ) , . ! + - <
+      else
+        value = chunk[0,1]
+        tokens << [value, value]
+        i += 1
+        
+      end
+      
+    end
+    
+    # Close all open blocks
+    while indent = indent_stack.pop
+      tokens << [:DEDENT, indent_stack.last || 0]
+    end
+    
+    tokens
+  end
 end
