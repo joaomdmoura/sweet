bab6e8b44dd50a21939623f53cc1cdb024a4b29d
João Moura <joaomdmoura@gmail.com>
2011-12-15 11:38:25 -0200

    Fixing a lot of issues

--- lib/lexer/strings.rb
+++ lib/lexer/strings.rb
@@ -1,10 +1,18 @@
 class Strings
 
-	def convert_string(code)
-		strings = code.scan(/\t(\"[a-zA-Z0-9].*\")/)
+	def convert_string(code, indent)
+		strings = code.scan(/\t("[^"]+")/m)
 		strings.uniq!
 		strings.each do |string|
-			new_string = "print(#{string[0]})"
+			original_tabs = code.scan(/((\t)+)#{string[0]}/)
+			original_tabs = (!original_tabs[0].nil?) ? original_tabs[0][0].count("\t") : 1
+			n_string = string[0].gsub( "<"," &lt;" ).gsub( ">"," &gt;" )
+			n_string = n_string.gsub(/\n/, "<br />")
+			get_tabs = n_string.scan(/((\t)+)/)
+			get_tabs.each do |line|
+				n_string = n_string.sub(/(\t)+/, "#{ '&nbsp;&nbsp;' * (line[0].count("\t") - original_tabs) }")
+			end
+			new_string = "print(#{n_string})"
 			code = code.gsub(string[0], new_string)
 		end
 		return code
--- lib/lexer/tags.rb
+++ lib/lexer/tags.rb
@@ -44,7 +44,7 @@ class Tags
 	def adding_attributes(code, tag)
 		_attr = [ [ "id", "#" ], [ "class", "\\." ] ]
 		_attr.each do |a|
-			orig = code.scan(/#{tag}([.a-zA-Z0-9=-_ ]+)? (#{a[1]}([a-zA-Z0-9]+))/)
+			orig = code.scan(/^#{tag}([.a-zA-Z0-9=-_ ]+)? (#{a[1]}([a-zA-Z0-9_\-]+))/)
 			orig.map! {|x| [x[1], x[2]] }
 			orig.uniq!
 			orig.each do |_old|
@@ -57,16 +57,16 @@ class Tags
 
 	def implement_tag(code, indent=1)
 		new_code = []
+		strings = Strings.new
+		code = strings.convert_string( code, indent )
 		@@inline_tags.each do |tag|
-			general = code.scan(/(^(\t)+?(#{tag}(()$| )([ #.=_\-a-zA-Z0-9]+)?((\n\t.*)+)?).*)/)
+			general = code.scan(/(^(\t)+?(#{tag}(()$| )([ #.=_\-a-zA-Z0-9:\/\?\+,]+)?))/)
 			general.map! {|x| x[0]}
 			general.each_with_index do |block, index|
-				new_indent = general[0].split("\t").count
+				new_indent = block.scan(/((\t)+)[^\t]/)[0][0].count("\t") + 1
 				_code_block = block
 				code_block = adding_attributes(block, tag)
-				strings = Strings.new
-				code_block = strings.convert_string( code_block )
-				atr = code_block.scan(/([a-zA-Z0-9_-]+=)'?"?([\.\/a-zA-Z0-9_-]+)'?"?/)
+				atr = code_block.scan(/([a-zA-Z0-9_-]+=)'?"?([\.\/:a-zA-Z0-9_-]+)'?"?/)		
 				code_block = convert_tag( code_block , index, new_indent, tag, atr, true)
 				code = code.gsub(_code_block, code_block)
 			end
@@ -78,8 +78,8 @@ class Tags
 				_code_block = block
 				code_block = adding_attributes(block, tag)
 				@@tags.each do |r_tag|
-					if !code_block.scan(/^(\t){#{indent}}(#{r_tag}(()$| )([a-zA-Z0-9= \t_\-#.\\'\/]+)?((\n\1{#{indent+1},})?(["#a= ()a-zA-Z0-9_.,\--\\'\/]+)?)+)/m).empty?
-						r_code = code_block.scan(/^(\t){#{indent}}(#{r_tag}(()$| )([a-zA-Z0-9= \t_\-#.\\'\/]+)?((\n\1{#{indent+1},})?(["#a= ()a-zA-Z0-9_.,\--\\'\/]+)?)+)/m)
+					if !code_block.scan(/^(\t){#{indent}}(#{r_tag}(()$| )([a-zA-Z0-9= \t_\-#.\\'\/]+)?((\n\1{#{indent+1},})?(["#a= ()a-zA-Z0-9_.,\--\\'\/&]+)?)+)/m).empty?
+						r_code = code_block.scan(/^(\t){#{indent}}(#{r_tag}(()$| )([a-zA-Z0-9= \t_\-#.\\'\/]+)?((\n\1{#{indent+1},})?(["#a= ()a-zA-Z0-9_.,\--\\'\/&]+)?)+)/m)
 						r_code.each do |r|
 							new_indent = indent + 1
 							new_block = implement_tag(r[1], new_indent)
@@ -87,9 +87,7 @@ class Tags
 						end
 					end
 				end
-				strings = Strings.new
-				code_block = strings.convert_string( code_block )
-				atr = code_block.scan(/([a-zA-Z0-9_-]+=)([\.\/a-zA-Z0-9_-]+)/)
+				atr = code_block.scan(/([a-zA-Z0-9_-]+=)([\.\/:a-zA-Z0-9_-]+)/)
 				code_block = convert_tag( code_block , index, indent, tag, atr)
 				code = code.gsub(_code_block, code_block)
 			end
