e78e9f71b6adabd26a798643feaee4239b960adc
João Moura <joaomdmoura@gmail.com>
2011-12-16 01:07:33 -0200

    adding correct class to fix position of layout

--- index.s
+++ index.s
@@ -5,7 +5,7 @@ html
 		link href=http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,700,600 rel=stylesheet type=text/css
 		link rel=stylesheet href=http://joaomdmoura.github.com/sweet/assets/css/style.css
 	body
-		a href=http://github.com/joaomdmoura/sweet .github
+		a class=github href=http://github.com/joaomdmoura/sweet
 			img .github src=https://a248.e.akamai.net/assets.github.com/img/6429057dfef9e98189338d22e7f6646c6694f032/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6f72616e67655f6666373630302e706e67 alt=Fork me on GitHub
 		div #content
 			div #header
